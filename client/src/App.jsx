e8db2bd5d3bad24b64d161f802bbe00a76837abc
abhinav3254 <abhinav3254@gmail.com>
2024-01-11 22:57:42 +0530

    routing and json generation done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './Pages/login/Login';
 import SignUp from './Pages/signup/SignUp';
+import Home from './Pages/Home/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/home' element={<Home />} />
         </Routes>
       </Router>
     </>
---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.scss'
+
+function Home() {
+    return (
+        <div>
+            <h1>WELCOME TO HOME PAGE</h1>
+        </div>
+    )
+}
+
+export default Home
