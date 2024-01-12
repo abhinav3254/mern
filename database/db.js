4cd5deaea6a757596aedd37d4c427bfb1627df7d
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2024-01-12 13:17:52 +0530

    signup done
    Query for Postgres is there in database folder

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'book',
+    database: 'mern',
     password: 'root3254',
     port: '5432',
     // Maximum number of clients in the pool 
